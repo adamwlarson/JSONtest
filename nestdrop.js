db674a682353c26201e304762fe3cef2817ba87a
matthewcarlreetz <matthewcarlreetz@gmail.com>
2014-02-07 14:00:14 -0600

    Update nestdrop.js

--- nestdrop.js
+++ nestdrop.js
@@ -4,85 +4,48 @@ var app = express();
 
 app.listen(3000);
 
-app.get('/getProducts', function(req,res) {
-	var jsonObj = {
-		status: "6000",
-		action: "getProducts",
-		categories: {
-			beer: [
-			],
-			wine: [
-			
-			],
-			liquor: [
-			
-			]
-		}
-	}
+app.get('/getProducts', function (req, res) {
+        var jsonObj = {
+            status: "6000",
+            action: "getProducts",
+            categories: [{
+                "name": "beer",
+                "iconUrl": "www.icon.com",
+                "products": []
+            }, {
+                "name": "beer",
+                "iconUrl": "www.icon.com",
+                "products": []
+            }, {
+                "name": "beer",
+                "iconUrl": "www.icon.com",
+                "products": []
+            }]
+        }
+        for (var i = 0; i < jsonObj.categories.length; i++) {
+            for (var j = 0; j < 10; j++) {
+                var newObj = {
+                    "imageProfileUrl": "www.google.com",
+                    "imageUrl": "www.google.com",
+                    "largeImageUrl": "www.google.com",
+                    "name": "ABSOLUTBLUE",
+                    "subtitle": "VODKA AHUS, SKANE,INSOUTHERNSWEDEN",
+                    "description": "this is some decent vodka",
+                    "qtyAvailable": 55,
+                    "size": 750,
+                    "unit": "ml",
+                    "type": "vodka",
+                    "keywords": [
+                        "vodka",
+                        "potatoes",
+                        "clear"
+                    ],
+                    "origin": "Sweden",
+                    "price": 16.75
+                }
+                jsonObj.categories[i].products.push(newObj);
+            }
 
+            res.json(jsonObj);
 
-	for( var i = 0; i < 10; i++ ) {
-		var newObj =             {
-                	"image-profile-url": "www.google.com",
-                	"image-url": "www.google.com",
-                	"large-image-url": "www.google.com",
-                	"name": "ABSOLUTBLUE",
-                	"subtitle": "VODKA AHUS, SKANE,INSOUTHERNSWEDEN",
-                	"description": "thisissomedecentvodka  Icouldgoforsomerightnow",
-                	"qty-available": 55,
-                	"size": 750,
-                	"unit": "ml",
-                	"type": "vodka",
-                	"keywords": [
-                    	"vodka",
-                    	"potatoes",
-                    	"clear"
-                	],
-                	"origin": "Sweden",
-                	"price": 16.75
-            	}
-		jsonObj.categories.beer.push( newObj );
-
-		var newObj2 =  {
-                	"image-profile-url": "www.google.com",
-                	"image-url": "www.google.com",
-                	"large-image-url": "www.google.com",
-                	"name": "ABSOLUTBLUE",
-                	"subtitle": "VODKA AHUS, SKANE,INSOUTHERNSWEDEN",
-                	"description": "thisissomedecentvodka  Icouldgoforsomerightnow",
-                	"qty-available": 55,
-                	"volume": "750ml",
-                	"type": "vodka",
-                	"keywords": [
-                    	"vodka",
-                    	"potatoes",
-                    	"clear"
-                	],
-                	"origin": "Sweden",
-                	"price": 16.75
-            	}
-		jsonObj.categories.wine.push( newObj2 );
-
-		var newObj3 =  {
-                	"image-profile-url": "www.google.com",
-                	"image-url": "www.google.com",
-                	"large-image-url": "www.google.com",
-                	"name": "ABSOLUTBLUE",
-                	"subtitle": "VODKA AHUS, SKANE,INSOUTHERNSWEDEN",
-                	"description": "thisissomedecentvodka  Icouldgoforsomerightnow",
-                	"qty-available": 55,
-                	"volume": "750ml",
-                	"type": "vodka",
-                	"keywords": [
-                    		"vodka",
-                    		"potatoes",
-                    		"clear"
-                	],
-                	"origin": "Sweden",
-                	"price": 16.75
-            	}
-		jsonObj.categories.liquor.push(newObj3);
-	}
-	res.json( jsonObj );
-			
-});
+        });
