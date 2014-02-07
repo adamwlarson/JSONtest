9b72d3b9be5ecc929184fdc97b0c06173083f385
matthewcarlreetz <matthewcarlreetz@gmail.com>
2014-02-07 14:27:31 -0600

    Update nestdrop.js

--- nestdrop.js
+++ nestdrop.js
@@ -5,47 +5,47 @@ var app = express();
 app.listen(3000);
 
 app.get('/getProducts', function (req, res) {
-        var jsonObj = {
-            status: "6000",
-            action: "getProducts",
-            categories: [{
-                "name": "beer",
-                "iconUrl": "www.icon.com",
-                "products": []
-            }, {
-                "name": "beer",
-                "iconUrl": "www.icon.com",
-                "products": []
-            }, {
-                "name": "beer",
-                "iconUrl": "www.icon.com",
-                "products": []
-            }]
-        }
-        for (var i = 0; i < jsonObj.categories.length; i++) {
-            for (var j = 0; j < 10; j++) {
-                var newObj = {
-                    "imageProfileUrl": "www.google.com",
-                    "imageUrl": "www.google.com",
-                    "largeImageUrl": "www.google.com",
-                    "name": "ABSOLUTBLUE",
-                    "subtitle": "VODKA AHUS, SKANE,INSOUTHERNSWEDEN",
-                    "description": "this is some decent vodka",
-                    "qtyAvailable": 55,
-                    "size": 750,
-                    "unit": "ml",
-                    "type": "vodka",
-                    "keywords": [
-                        "vodka",
-                        "potatoes",
-                        "clear"
-                    ],
-                    "origin": "Sweden",
-                    "price": 16.75
-                }
-                jsonObj.categories[i].products.push(newObj);
+    var jsonObj = {
+        status: "6000",
+        action: "getProducts",
+        categories: [{
+            "name": "beer",
+            "iconUrl": "www.icon.com",
+            "products": []
+        }, {
+            "name": "beer",
+            "iconUrl": "www.icon.com",
+            "products": []
+        }, {
+            "name": "beer",
+            "iconUrl": "www.icon.com",
+            "products": []
+        }]
+    }
+    for (var i = 0; i < jsonObj.categories.length; i++) {
+        for (var j = 0; j < 10; j++) {
+            var newObj = {
+                "imageProfileUrl": "www.google.com",
+                "imageUrl": "www.google.com",
+                "largeImageUrl": "www.google.com",
+                "name": "ABSOLUTBLUE",
+                "subtitle": "VODKA AHUS, SKANE,INSOUTHERNSWEDEN",
+                "description": "this is some decent vodka",
+                "qtyAvailable": 55,
+                "size": 750,
+                "unit": "ml",
+                "type": "vodka",
+                "keywords": [
+                "vodka",
+                "potatoes",
+                "clear"
+                ],
+                "origin": "Sweden",
+                "price": 16.75
             }
+            jsonObj.categories[i].products.push(newObj);
+        }
+    }
+    res.json(jsonObj);
 
-            res.json(jsonObj);
-
-        });
+});
